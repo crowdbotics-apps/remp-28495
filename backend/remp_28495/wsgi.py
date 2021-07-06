f2430f3b7dd53d9ff43465fe12da1dd28925e773
Crowdbotics <team@crowdbotics.com>
2021-07-06 16:45:51 +0000

    Initial commit.

---
+++ backend/remp_28495/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for remp_28495 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'remp_28495.settings')
+
+application = get_wsgi_application()
